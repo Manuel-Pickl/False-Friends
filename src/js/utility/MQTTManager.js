dfb6832723a828335545eaa717f4b919cdd75465
HansHar <85882579+HansHar@users.noreply.github.com>
2022-07-05 21:03:27 +0200

    corrected subscibe topic

--- src/js/utility/MQTTManager.js
+++ src/js/utility/MQTTManager.js
@@ -21,7 +21,7 @@
      */
     constructor() {
         this.brokerLog = document.querySelector(".modal .settings .broker-log");
-        this.subTopic = "sensor/data_MP";
+        this.subTopic = "sensor/data";
         this.pubTopic = "sensehat/message";
         this.subscribed = false;
         this.connected = false;
